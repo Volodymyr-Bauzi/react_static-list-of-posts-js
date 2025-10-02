334adbd1b3351004baf7c8c5acaeb5c61909bbb8
Bauzi <volodiasyd@gmail.com>
2025-10-02 17:35:08 +0300

    Solution #5

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -4,12 +4,8 @@ export const UserInfo = ({ user }) => (
   <p>
     {' Posted by  '}
 
-    {user ? (
-      <a className="UserInfo" href={`mailto:${user.email}`}>
-        {user.name}
-      </a>
-    ) : (
-      'Unknown'
-    )}
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
   </p>
 );
